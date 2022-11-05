7f2d2bdea0f9939cd2166488f7625ee4f0d42534
G0uth4m <goutham67r@gmail.com>
2022-11-05 12:49:31 +0530

    project setup

---
+++ src/main/java/com/goutham/mediumblogservice/MediumBlogServiceApplication.java
@@ -0,0 +1,13 @@
+package com.goutham.mediumblogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MediumBlogServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MediumBlogServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/goutham/mediumblogservice/MediumBlogServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.goutham.mediumblogservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MediumBlogServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
